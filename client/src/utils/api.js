8c4e2ddfe451e2281d0e187640d4ff733ed337e5
madhavaneja24 <157693718+madhavaneja24@users.noreply.github.com>
2026-05-22 09:49:14 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,3 +1,3 @@
-const BASE_URL = process.env.REACT_APP_API_URL || "https://resumify-server-jdxo.onrender.com";
+const BASE_URL = "https://resumify-server-jdxo.onrender.com";
 
 export default BASE_URL;
